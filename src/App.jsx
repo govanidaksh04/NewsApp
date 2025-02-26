f3bd22008b9d777b39b677845bf946786462e4ab
Daksh <govanidaksh@gmail.com>
2025-02-26 21:38:36 +0530

    ABC

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Navbar from "./Components/Navbar.jsx";
-import NewsBoard from "./Components/newsboard.jsx";
+import NewsBoard from "./Components/NewsBoard.jsx";
 
 function App() {
   const [category, setCategory] = useState("general");
